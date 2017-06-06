ef53fce0b6de0a2ea786412deba9af9f68371039
Chase Elander <chaseelander@gmail.com>
2017-06-06 02:05:13 -0500

    outputs list of lists

--- scrape.py
+++ scrape.py
@@ -9,6 +9,12 @@ html = response.content
 soup = BeautifulSoup(html)
 table = soup.find('tbody', attrs={'class':'stripe'})
 
+list_of_rows = []
 for row in table.findAll('tr'):
+	list_of_cells = []
 	for cell in row.findAll('td'):
-		print cell.text.replace('&nbsp;', '')
+		text = cell.text.replace('&nbsp;', '')
+		list_of_cells.append(text)
+	list_of_rows.append(list_of_cells)
+
+print list_of_rows
